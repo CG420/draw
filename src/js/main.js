3662bb6696adeb7001cc0c5239fb62eb10549fd9
Chris Grooms <cgrooms21@aol.com>
2019-04-02 10:24:15 -0500

    draw

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,15 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-ctx.fillStyle = '#' + Math.floor(Math.random()*16777215).toString(16);
-ctx.fillRect(10, 10, 100, 100);
+var x;
+var y;
+
+setInterval(jump, 1000)
+
+function jump(){
+    ctx.clearRect(0, 0, 1000, 1000);
+    x = Math.random() * 700;
+    y = Math.random() * 500;
+    ctx.fillStyle = '#' + Math.floor(Math.random()*16777215).toString(16);
+    ctx.fillRect(10, 10, 100, 100);
+
+}
